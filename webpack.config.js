a8861d1de15239ee356439285f2327c49ffd9670
Enzo García <eramirez@sugit.com.ar>
2018-11-02 12:19:59 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + '/build',
+        path: path.resolve(__dirname, '..', 'build'),
         filename: 'bundle.js',
         publicPath: '/'
     },
